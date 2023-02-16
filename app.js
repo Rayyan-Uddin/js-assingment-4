d24bfe333035ddd2b5fa252a32882e4581fdeab1
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-02-16 18:26:13 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,247 @@
+// Js assingment 4
+
+// Chapter 9 - 10
+
+// ------------------------------------start-----------------------------------
+
+
+// Question 1
+
+// var city = prompt("Enter your city:");
+//     x = "Welcom to the city of lights."
+// if(city ==="karachi"){
+//     alert(x)
+// }
+// else{
+//     alert("welcom to "+city)
+// }
+
+
+// Question 2
+
+// var gender = prompt("Enter your Gender:");
+//     x = "Good morning Sir!";
+//     y = "Good morning Ma'am!";
+
+// if (gender ==="male"){
+//     alert(x)
+// }
+
+// else if (gender ==="female"){
+//     alert(y)
+// }
+// else{
+//     alert("Phir kon h tu?")
+// }
+
+
+// Quesiton 3
+
+// var red = "Must Stop"
+// var yellow = "Ready to Move"
+// var green = "Move"
+
+//     signal = prompt("Enter a Signal Light:");
+
+// if(signal === "red"){
+//     alert(red)
+// }
+// else if(signal === "yellow"){
+//     alert(yellow)
+// }
+// else if(signal === "green"){
+//     alert(green)
+// }
+// else{
+//     alert("wrong answer!")
+// }
+
+
+// Quesiton 4
+
+// var fuel = prompt("Amount of Petrol in litres");
+
+// if (fuel <= 0.25){
+//     alert("Please refill the fuel in your car.")
+// }
+// else{
+//     alert("Go on")
+// }
+
+
+// Question 5
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// // User Input & Conditional Statement | JAVASCRIPT
+
+// // Page 2 of 4
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// // User Input & Conditional Statement | JAVASCRIPT
+
+// // Page 3 of 4
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// Quesstion 6
+
+// var sub1 = +prompt("Enter Obtained Marks in Subject 1:")
+//     sub2 = +prompt("Enter Obtained Marks in Subject 2:")
+//     sub3 = +prompt("Enter Obtained Marks in Subject 3:")
+//     totalMarks = prompt("Enter Total Marks:")
+//     marksObtained = sub1 + sub2 + sub3;
+//     percentage = (marksObtained / totalMarks) * 100
+
+// if (percentage >= 80){
+//     grade = "A-one"
+//     remarks = "Excellent"
+// document.write( "<h1/>Marks Sheet" + "<br/> Total Marks = " + totalMarks+ "<br/> Obtained Marks = " + marksObtained
+// + "<br/> Percentage = " + percentage + "<br/> Grade = "+ grade + "<br /> Remarks = " + remarks)
+// }
+
+
+// else if (percentage >= 70){
+//     grade = "A"
+//     remarks = "Good"
+// document.write( "<h1/>Marks Sheet" + "<br/> Total Marks = " + totalMarks+ "<br/> Obtained Marks = " + marksObtained
+// + "<br/> Percentage = " + percentage + "<br/> Grade = "+ grade + "<br /> Remarks = " + remarks)
+// }
+
+
+// else if (percentage >= 60){
+//     grade = "B"
+//     remarks = "You need to Improve"
+// document.write( "<h1/>Marks Sheet" + "<br/> Total Marks = " + totalMarks+ "<br/> Obtained Marks = " + marksObtained
+// + "<br/> Percentage = " + percentage + "<br/> Grade = "+ grade + "<br /> Remarks = " + remarks)
+// }
+
+
+// else if (percentage < 60){
+//     grade = "Fail"
+//     remarks = "Sorry"
+// document.write( "<h1/>Marks Sheet" + "<br/> Total Marks = " + totalMarks+ "<br/> Obtained Marks = " + marksObtained
+// + "<br/> Percentage = " + percentage + "<br/> Grade = "+ grade + "<br /> Remarks = " + remarks)
+// }
+
+
+// Question 7
+
+// var num = 7;
+
+//     guessNum = +prompt("Enter your number:");
+
+// if (guessNum === num){
+//     alert("Bingo! Correct Answer")
+// }
+// else if (guessNum === 6 ) {
+//     alert("Close enough to the Correct answer.")
+// }
+// else if (guessNum === 8 ) {
+//     alert("Close enough to the Correct answer.")
+// }
+// else {
+//     alert("Wrong Answer")
+// }
+
+
+// Quesiton 8
+
+// var num = +prompt("Enter any Number:")
+
+// if ( num % 3 == 0 ){
+//     alert("Your Number is Divisble by 3")
+// }
+// else{
+//     alert("Your Number is not Divisble by 3")
+// }
+
+
+// Question 9
+
+
+// var num = +prompt("Enter any Number:")
+
+// if ( num % 2 == 0 ){
+//     alert("Your Number is an Even number")
+// }
+// else{
+//     alert("Your Number is an Odd number")
+// }
+
+
+// Questoin 10
+
+// var tem = +prompt("Enter the temperature:")
+
+// if (tem > 40){
+//     alert("It's too hot outside.")
+// }
+// else if (tem > 30){
+//     alert("The weather today is normal.")
+// }
+// else if (tem > 20){
+//     alert("Today's weather is cool.")
+// }
+// else if (tem > 10){
+//     alert("OMG! Today's weather is so cool.")
+// }
+
+
+// Question 11
+
+// var num1 = +prompt("Enter 1st Number:")
+// var num2 = +prompt("Enter 2nd Number:")
+//     operator = prompt("Enter Operation + , - , / , * , % .")
+    
+// if (operator === "+" ){
+//     alert(num1 + num2)
+// }
+// if (operator === "-" ){
+//     alert(num1 - num2)
+// }
+// if (operator === "/" ){
+//     alert(num1 / num2)
+// }
+// if (operator === "*"){
+//     alert(num1 * num2)
+// }
+// if (operator === "%"){
+//     alert(num1 % num2)
+// }
+// ----------------------------------END--------------------------------
